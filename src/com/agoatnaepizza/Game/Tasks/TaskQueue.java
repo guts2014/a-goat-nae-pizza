7c36661929108fb47a5bcaea2006a1a25cf3d0e6
skDn <2038011Y@student.gla.ac.uk>
2014-10-11 11:17:26 +0100

    update on task queue

--- src/com/agoatnaepizza/Game/Tasks/TaskQueue.java
+++ src/com/agoatnaepizza/Game/Tasks/TaskQueue.java
@@ -1,9 +1,41 @@
 package com.agoatnaepizza.Game.Tasks;
 
+import java.util.ArrayDeque;
+
+import com.agoatnaepizza.Game.Tasks.Customer.CustomerType;
+
 /**
  * User: nishad
  * Date: 10/10/14
  * Time: 23:53
  */
-public class TaskQueue {
+public class TaskQueue {	
+	ArrayDeque emailQue;
+	ArrayDeque callQue;
+	ArrayDeque socialQue;
+	
+	int calltime=15;
+	int emailtime=10;
+	int socialtime=5;
+	public TaskQueue(){
+		
+	}
+	
+	
+	//implement random customers
+	public Customer getRandomEmailCustomer(){
+		Customer newCustomer=new Customer(CustomerType.email, false, calltime, calltime, calltime, null);
+		return null;
+		
+	}
+	public Customer getRandomCallCustomer(){
+		Customer newCustomer=new Customer(CustomerType.call, false, calltime, calltime, calltime, null);
+		return null;
+		
+	}
+	public Customer getRandomSocCustomer(){
+		Customer newCustomer=new Customer(CustomerType.soc, false, calltime, calltime, calltime, null);
+		return null;
+		
+	}
 }
